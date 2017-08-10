fab408388aa4013a33eaeec96a989b66f8040c55
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-10 15:33:54 +0200

    AK bot

--- api.py
+++ api.py
@@ -34,11 +34,11 @@ def api_message(text,user_id):
     speech=res['result']['fulfillment']['speech']
     intention=res['result']['action']
     #try:
-        if intention=="gallery":
-            fbweb.get_gallery(config.HID,"it_IT",config.H_Access_Token)
-            return ([speech]+[intention])
-        else:
-            return ([speech]+[intention])
+    if intention=="gallery":
+        fbweb.get_gallery(config.HID,"it_IT",config.H_Access_Token)
+        return ([speech]+[intention])
+    else:
+        return ([speech]+[intention])
     #except:
     #    print (speech,intention)
     #    return ([speech]+[intention])
