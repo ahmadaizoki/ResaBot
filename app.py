f09632d4f2259818be91025f37093ed1a1659985
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-10 17:09:01 +0200

    AK bot

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def fb_receive_message():
                         print (alt)
                         template=Template.Generic([])
                         for i in range (0,ln):
-                            template=Template.Generic([
+                            template=Template([
                                 Template.GenericElement("Gallery",
                                   item_url=url[i],
                                   image_url=url[i],
