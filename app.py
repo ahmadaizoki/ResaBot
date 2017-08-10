670a3fbeb3b0364d0c23f57de670546878b32e60
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-10 17:16:13 +0200

    AK bot

--- app.py
+++ app.py
@@ -54,13 +54,13 @@ def fb_receive_message():
                         print (alt)
                         template=Template.Generic([])
                         for i in range (0,ln):
-                            template=template+[Template.Generic([
+                            template=template(
                                 Template.GenericElement("Gallery",
                                   item_url=url[i],
                                   image_url=url[i],
                                   subtitle=alt[i]
                                 )
-                            ])]
+                            )
                         page.send(user_id,template)
                         #client.send_text(user_id,"speech")
                     else:
