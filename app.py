0eaf2e2afe925d98cdce5a4cdc7329f2bce357ae
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-11 10:38:19 +0200

    Ak bot

--- app.py
+++ app.py
@@ -88,20 +88,20 @@ def fb_receive_message():
                                 template=template+[Template.GenericElement(title[i],
                                 item_url=url[i],
                                 image_url=url[i],
-                                subtitle="DU "+q_from+" au "+q_to,
-                                text="Reserver a partir de "+q_price+" "+q_currency,
+                                subtitle="DU "+q_from[i]+" au "+q_to[i],
+                                text="Reserver a partir de "+q_price[i]+" "+q_currency[i],
                                 buttons=[
-                                Template.ButtonWeb("Reserver",q_BookLink)
+                                Template.ButtonWeb("Reserver",q_BookLink[i])
                                 ])]
                         else:
                             for i in range (0,ln):
                                 template=template+[Template.GenericElement(title[i],
                                 item_url=url[i],
                                 image_url=url[i],
-                                subtitle="DU "+q_from+" au "+q_to,
-                                text="Reserver a partir de "+q_price+" "+q_currency,
+                                subtitle="DU "+q_from[i]+" au "+q_to[i],
+                                text="Reserver a partir de "+q_price[i]+" "+q_currency[i],
                                 buttons=[
-                                Template.ButtonWeb("Reserver",q_BookLink)
+                                Template.ButtonWeb("Reserver",q_BookLink[i])
                                 ])]
                         page.send(user_id,Template.Generic(template))
                     else:
