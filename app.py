12a645973bcbb36782cc59376f72c4a5e504119e
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-21 11:01:31 +0200

    bot

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def fb_receive_message():
                             subtitle="Pour "+str(q_nights)+" nuits et "+str(q_adults)+" personne(s)"+"\n"+"Du "+q_from+" au "+q_to+" à partir de "+str(q_price)+" "+q_currency+" par chambre",
                             buttons=[
                             Template.ButtonWeb("Réserver",q_BookLink),
-                            Template.ButtonPostBack("Plus de chambres","CHAMBRE_PAYLOAD")#more_room(user_id,date,"",nights,adults,conf.HID,"json","",conf.H_Access_Token))
+                            Template.ButtonPostBack("Plus de chambres","CHAMBRE_PAYLOAD"+date+nights+adults)#more_room(user_id,date,"",nights,adults,conf.HID,"json","",conf.H_Access_Token))
                             ])]
                             page.send(user_id,Template.Generic(template))
                         except:
