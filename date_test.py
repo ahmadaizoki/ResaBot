7e7326cf4c7b40bc12cb58b3f1a1512c8849438a
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-22 10:11:00 +0200

    bot

--- date_test.py
+++ date_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
+import datetime
 
 def test(date):
     date=str(date)
@@ -12,6 +13,7 @@ def test(date):
             time.strptime(date,'%d-%m-%Y')
             d=date.split("-")
             date=str(d[2])+"-"+str(d[1])+"-"+str(d[0])
+            date=datetime.date(int(d[2]),int(d[1]),int(d[0]))
             print (date)
             print (d[0])
         except:
