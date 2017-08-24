d4167974f57259d3ee6d53ae4b4eabdc51ac8766
ahmadaizoki <ahmadaizoki@githup.com>
2017-08-24 16:03:04 +0200

    bot

--- date_jour_semaine.py
+++ date_jour_semaine.py
@@ -5,37 +5,37 @@ import datetime
 def jour_prochain(sentence):
     d=datetime.datetime.today()
     dd=d.weekday()
-    if sentence=="lundi prochain" or sentence=="prochain lundi" or sentence=="pour lundi prochain" or sentence=="pour prochain lundi":
+    if sentence=="lundi prochain" or sentence=="prochain lundi" or sentence=="pour lundi prochain" or sentence=="pour prochain lundi" or sentence=="lundi":
         if dd==0:
             res=7
         else:
             res=(7-dd+0)%7
-    elif sentence=="mardi prochain" or sentence=="prochain mardi" or sentence=="pour mardi prochain" or sentence=="pour prochain mardi":
+    elif sentence=="mardi prochain" or sentence=="prochain mardi" or sentence=="pour mardi prochain" or sentence=="pour prochain mardi" or sentence=="mardi":
         if dd==1:
             res=7
         else:
             res=(7-dd+1)%7
-    elif sentence=="mercredi prochain" or sentence=="prochain mercredi" or sentence=="pour mercredi prochain" or sentence=="pour prochain mercredi":
+    elif sentence=="mercredi prochain" or sentence=="prochain mercredi" or sentence=="pour mercredi prochain" or sentence=="pour prochain mercredi" or sentence=="mercredi":
         if dd==2:
             res=7
         else:
             res=(7-dd+2)%7
-    elif sentence=="jeudi prochain" or sentence=="prochain jeudi"or sentence=="pour jeudi prochain" or sentence=="pour prochain jeudi":
+    elif sentence=="jeudi prochain" or sentence=="prochain jeudi"or sentence=="pour jeudi prochain" or sentence=="pour prochain jeudi" or sentence=="jeudi":
         if dd==3:
             res=7
         else:
             res=(7-dd+3)%7
-    elif sentence=="vendredi prochain" or sentence=="prochain vendredi" or sentence=="pour vendredi prochain" or sentence=="pour prochain vendredi":
+    elif sentence=="vendredi prochain" or sentence=="prochain vendredi" or sentence=="pour vendredi prochain" or sentence=="pour prochain vendredi" or sentence=="vendredi":
         if dd==4:
             res=7
         else:
             res=(7-dd+4)%7
-    elif sentence=="samedi prochain" or sentence=="prochain samedi" or sentence=="pour samedi prochain" or sentence=="pour prochain samedi":
+    elif sentence=="samedi prochain" or sentence=="prochain samedi" or sentence=="pour samedi prochain" or sentence=="pour prochain samedi" or sentence=="samedi":
         if dd==5:
             res=7
         else:
             res=(7-dd+5)%7
-    elif sentence=="dimanche prochain" or sentence=="prochain dimanche" or sentence=="pour dimanche prochain" or sentence=="pour prochain dimanche":
+    elif sentence=="dimanche prochain" or sentence=="prochain dimanche" or sentence=="pour dimanche prochain" or sentence=="pour prochain dimanche" or sentence=="dimanche":
         if dd==6:
             res=7
         else:
