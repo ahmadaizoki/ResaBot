9e37a62e5e48d2de54434a86b3b725ff2b256c1b
ahmadaizoki <ahmadaizoki@githup.com>
2017-09-01 12:45:15 +0200

    bot

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def fb_receive_message():
     for entry in message_entries:
         for message in entry['messaging']:
             if message.get('message'):
+                print (message)
                 try:
                     user_id="{sender[id]}".format(**message)
                     text="{message[text]}".format(**message)
