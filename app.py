b0f8b50b474272853d482ccb8b067ec2475f45db
ahmadaizoki <ahmadaizoki@githup.com>
2017-09-12 13:59:08 +0200

    bot

--- app.py
+++ app.py
@@ -185,6 +185,7 @@ def fb_receive_message():
                                         res_offre_nom=res_offre[1]
                                         print (res_offre_of)
                                         if res_offre_of=="p2":
+                                            print ("je suis la !")
                                             offre_week=offre_weekend.offre_we(q_from,q_to,q_nights,q_adults)
                                             print (offre_week)
                                             if offre_week!="":
